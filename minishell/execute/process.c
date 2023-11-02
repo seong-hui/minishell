af3b79d0b04ab66c702062b4c1c345d3ff633ef7
Seonghui Moon <seonghmo@c1r8s2.42seoul.kr>
2023-11-02 22:13:49 +0900

    redirection norm

--- minishell/execute/process.c
+++ minishell/execute/process.c
@@ -6,32 +6,32 @@
 /*   By: seonghmo <seonghmo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:23:48 by seonghmo          #+#    #+#             */
-/*   Updated: 2023/11/02 21:43:10 by seonghmo         ###   ########.fr       */
+/*   Updated: 2023/11/02 21:51:09 by seonghmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/execute.h"
 
-void first_child(t_process *process, int *cur_fd, t_env *env, t_excute e_info)
+void	first_child(t_process *proc, int *cur_fd, t_env *env, t_excute e_info)
 {
 	close(cur_fd[0]);
-	if (process->infile_fd > 0)
-		dup2(process->infile_fd, STDIN_FILENO);
-	if (process->outfile_fd > 1)
-		dup2(process->outfile_fd, STDOUT_FILENO);
+	if (proc->infile_fd > 0)
+		dup2(proc->infile_fd, STDIN_FILENO);
+	if (proc->outfile_fd > 1)
+		dup2(proc->outfile_fd, STDOUT_FILENO);
 	else
 		dup2(cur_fd[1], STDOUT_FILENO);
-	if (is_builtin(process))
+	if (is_builtin(proc))
 	{
-		check_builtins(process, env, cur_fd[1]);
+		check_builtins(proc, env, cur_fd[1]);
 		close(cur_fd[1]);
 		exit(0);
 	}
 	else
 	{
 		close(cur_fd[1]);
-		if (execve(process->cmd_path, process->cmd, e_info.envp) == -1)
-			print_command_error(process->cmd[0]);
+		if (execve(proc->cmd_path, proc->cmd, e_info.envp) == -1)
+			print_command_error(proc->cmd[0]);
 	}
 }
 
--- minishell/execute/redirection.c
+++ minishell/execute/redirection.c
@@ -6,56 +6,68 @@
 /*   By: seonghmo <seonghmo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:15:18 by seonghmo          #+#    #+#             */
-/*   Updated: 2023/10/31 22:45:02 by seonghmo         ###   ########.fr       */
+/*   Updated: 2023/11/02 22:13:09 by seonghmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/execute.h"
 
-void fd_redirection(t_process *process, t_redir *redir)
+void	redir_input(t_process *process, t_redir *redir)
+{
+	process->infile_fd = open(redir->file, O_RDONLY);
+	if (process->infile_fd == -1)
+	{
+		perror("Failed to open infile");
+		exit(1);
+	}
+}
+
+void	redir_heredoc(t_process *process, t_redir *redir)
+{
+	process->infile_fd = open(redir->tmp, O_RDONLY);
+	if (process->infile_fd == -1)
+	{
+		perror("Failed to open infile");
+		exit(1);
+	}
+	unlink(redir->tmp);
+}
+
+void	redir_output(t_process *process, t_redir *redir)
+{
+	process->outfile_fd = open(redir->file, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (process->outfile_fd == -1)
+	{
+		perror("Failed to open outfile");
+		exit(1);
+	}
+}
+
+void	redir_append(t_process *process, t_redir *redir)
+{
+	process->outfile_fd = open(redir->file, O_RDWR | O_CREAT | O_APPEND, 0644);
+	if (process->outfile_fd == -1)
+	{
+		perror("Failed to open outfile");
+		exit(1);
+	}
+}
+
+void	fd_redirection(t_process *process, t_redir *redir)
 
 {
 	process->infile_fd = 0;
 	process->outfile_fd = 1;
 	while (redir)
 	{
-		if (redir->type == T_REDIR_INPUT) // infile이 있을 때
-		{
-			process->infile_fd = open(redir->file, O_RDONLY);
-			if (process->infile_fd == -1)
-			{
-				perror("Failed to open infile");
-				exit(1);
-			}
-		}
-		else if (redir->type == T_REDIR_HEREDOC && process->cmd[0]) // infile이 있을 때
-		{
-			process->infile_fd = open(redir->tmp, O_RDONLY);
-			if (process->infile_fd == -1)
-			{
-				perror("Failed to open infile");
-				exit(1);
-			}
-			unlink(redir->tmp);
-		}
-		else if (redir->type == T_REDIR_OUTPUT) // outfile이 있을 때
-		{
-			process->outfile_fd = open(redir->file, O_RDWR | O_CREAT | O_TRUNC, 0644);
-			if (process->outfile_fd == -1)
-			{
-				perror("Failed to open outfile");
-				exit(1);
-			}
-		}
-		else if (redir->type == T_REDIR_APPEND) // APPEND 처리 따로 해야함
-		{
-			process->outfile_fd = open(redir->file, O_RDWR | O_CREAT | O_APPEND, 0644);
-			if (process->outfile_fd == -1)
-			{
-				perror("Failed to open outfile");
-				exit(1);
-			}
-		}
+		if (redir->type == T_REDIR_INPUT)
+			redir_input(process, redir);
+		else if (redir->type == T_REDIR_HEREDOC)
+			redir_heredoc(process, redir);
+		else if (redir->type == T_REDIR_OUTPUT)
+			redir_output(process, redir);
+		else if (redir->type == T_REDIR_APPEND)
+			redir_append(process, redir);
 		redir = redir->next;
 	}
 }
