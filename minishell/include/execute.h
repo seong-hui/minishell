a8e981e6c3ea2875464bbbe0906d69ccf9dbb6e6
Seonghui Moon <seonghmo@c1r8s6.42seoul.kr>
2023-11-05 20:10:01 +0900

    builtin and execute header norm

--- minishell/include/execute.h
+++ minishell/include/execute.h
@@ -6,7 +6,7 @@
 /*   By: seonghmo <seonghmo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 21:26:42 by moonseonghu       #+#    #+#             */
-/*   Updated: 2023/11/04 15:24:34 by seonghmo         ###   ########.fr       */
+/*   Updated: 2023/11/05 20:07:23 by seonghmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 # define EXECUTE_H
 
 # include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <fcntl.h>
 # include <sys/wait.h>
 # include "parse.h"
 
@@ -33,22 +30,18 @@ void	check_builtins(t_process *process, t_env *env, int fd, t_excute e_info);
 int		is_builtin(t_process *process);
 int		ft_lstsize(t_process *lst);
 void	check_heredoc(t_process *process);
-void fd_redirection(t_process *process, t_redir *redir);
+void	fd_redirection(t_process *process, t_redir *redir);
 void	make_pipe(t_process *process, t_env *env, t_excute exe_info);
-char *get_cmd(char **path, char *cmd);
-char **get_path(t_env *env);
-void first_child(t_process *process, int *cur_fd, t_env *env, t_excute e_info);
-void	middle_child(t_process *process, int *cur_fd, t_env *env, t_excute e_info);
-void last_child(t_process *process, t_env *env, t_excute e_info);
-//void	wait_process(int pid);
+char	*get_cmd(char **path, char *cmd);
+char	**get_path(t_env *env);
+void	first_child(t_process *proc, int *cur_fd, t_env *env, t_excute e_info);
+void	middle_child(t_process *proc, int *cur_fd, t_env *env, t_excute e_info);
+void	last_child(t_process *process, t_env *env, t_excute e_info);
 void	exit_code_handler(int status, int *flag_printed, int pid, int child);
 void	print_file_error(char *file);
 void	print_command_error(char *cmd);
 void	wait_child(int i, int exit_code);
-int	child_exit_status(int exit_code);
-
-
-
-void heredoc_signal(void);
+int		child_exit_status(int exit_code);
+void	heredoc_signal(void);
 
 #endif
